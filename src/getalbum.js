d5c5f423c66ffa83e6278d045c7348ec40cd6453
webbin <1358102877@qq.com>
2023-08-20 17:12:44 +0800

    Update getalbum.js

--- src/getalbum.js
+++ src/getalbum.js
@@ -2,18 +2,19 @@ const axios = require("axios");
 
 const URL = "https://mp.weixin.qq.com/mp/appmsgalbum";
 
-const MetroStations = ["大学城", "深圳北"];
+const MetroStations = ["大学城", "深圳北", "民治"];
 
 // 2023-04-16 23:54:55
 const SearchTimeEnd = 1681660495;
 
-const generateParams = (startIndex = 0, count = 10) => {
+const generateParams = (msgid, itemidx, count = 10) => {
   const data = {
     action: "getalbum",
     __biz: "Mzg2MjgyMzI2OA==",
     album_id: "2830997749436465153",
     count,
-    begin_itemidx: startIndex,
+    begin_msgid: msgid,
+    begin_itemidx: itemidx,
     __biz: "Mzg2MjgyMzI2OA%3D%3D",
     x5: "0",
     f: "json",
@@ -25,8 +26,13 @@ const generateParams = (startIndex = 0, count = 10) => {
   return list.join("&");
 };
 
-const sendRequest = (startIndex = 0, count = 10) => {
-  const url = `${URL}?${generateParams(startIndex, count)}`;
+const sectionFilter = async (url) => {
+  const res = await axios.default.get(url);
+  console.log(res.data);
+};
+
+const sendRequest = (msgid, itemidx, count = 10) => {
+  const url = `${URL}?${generateParams(msgid, itemidx, count)}`;
   console.log("url = ", url);
   return axios.default.get(url, {
     headers: {},
@@ -56,46 +62,52 @@ const sendRequest = (startIndex = 0, count = 10) => {
 
 const searchArticals = async () => {
   let endtime = -1;
-  let index = 0;
+  let idx = "";
+  let msgid = "";
+  const count = 20;
 
-  const articals = []
+  const articals = [];
 
   try {
     while (endtime === -1 || endtime > SearchTimeEnd) {
-      const res = await sendRequest(index, 20);
+      const res = await sendRequest(msgid, idx, count);
       const list = res.data.getalbum_resp.article_list;
       if (!list || !list.length) {
         console.log(res.data);
       }
 
       const last = list[list.length - 1];
+      // console.log('last = ', last);
 
       endtime = parseInt(last.create_time, 10);
       list.forEach((item) => {
-        const { title = '' } = item;
-  
+        const { title = "" } = item;
+        // console.log('title = ', title);
+
         let isTarget = false;
         for (let i = 0; i < MetroStations.length; i += 1) {
           const station = MetroStations[i];
           if (title.indexOf(station) >= 0) {
             articals.push(item);
+            console.log("title = ", title);
+            console.log("url = ", item.url);
             isTarget = true;
             break;
           }
         }
-      })
+      });
 
-      index += 20;
+      msgid = last.msgid;
+      idx = last.itemidx;
     }
 
-
-    console.log(articals);
-
+    // console.log(articals);
   } catch (error) {
     console.log(error);
   }
 };
 
+searchArticals();
 
-
-searchArticals()
+// const url = "http://mp.weixin.qq.com/s?__biz=Mzg2MjgyMzI2OA==&mid=2247557471&idx=5&sn=b5a85ce1923a975e00faa388620f33e4&chksm=ce025754f975de427be7ecf01f556a7e490e8ca410cf2d45f16dcedc6ef89c8c35c992666f09#rd";
+// sectionFilter(url)
